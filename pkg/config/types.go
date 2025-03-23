6330852777959fd9430a19fd9512079d8c530256
vwency <duckdodo666@gmail.com>
2025-03-23 22:05:12 +0600

    added service_name in types and config

--- pkg/config/types.go
+++ pkg/config/types.go
@@ -5,5 +5,6 @@ type ServiceConfig struct {
 		Env      string `mapstructure:"env"`
 		Port     string `mapstructure:"port"`
 		LogLevel string `mapstructure:"log_level"`
+		service_name string `mapstructure:"service_name"`
 	}
 }
