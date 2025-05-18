793af8a440f8be4cb48b7da1eedfad4e0b29fd81
vwency <duckdodo666@gmail.com>
2025-05-18 18:53:36 +0600

    chore: keep-alive auth-service

--- cmd/auth_service/go-kit/auth-transport.go
+++ cmd/auth_service/go-kit/auth-transport.go
@@ -2,25 +2,39 @@ package gokit
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/vwency/microservices_golang/internal/auth_service/endpoints"
 	"github.com/vwency/microservices_golang/internal/auth_service/transport"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
 )
 
 func NewGRPCServer(
 	endpoints endpoints.Endpoints,
 	tlsCredentials credentials.TransportCredentials,
 ) *grpc.Server {
+	var opts []grpc.ServerOption
+
 	if tlsCredentials == nil {
 		fmt.Println("Warning: Running server without TLS")
-		server := grpc.NewServer()
-		transport.RegisterGRPCServer(server, endpoints)
-		return server
+	} else {
+		opts = append(opts, grpc.Creds(tlsCredentials))
 	}
 
-	server := grpc.NewServer(grpc.Creds(tlsCredentials))
+	// Keepalive параметры для улучшения производительности
+	kaParams := keepalive.ServerParameters{
+		MaxConnectionIdle:     15 * time.Minute, // закрывать неактивные соединения через 15 минут
+		MaxConnectionAge:      24 * time.Hour,   // максимальный возраст соединения
+		MaxConnectionAgeGrace: 5 * time.Minute,  // grace period перед закрытием
+		Time:                  10 * time.Minute, // время отправки ping клиенту
+		Timeout:               20 * time.Second, // время ожидания pong от клиента
+	}
+
+	opts = append(opts, grpc.KeepaliveParams(kaParams))
+
+	server := grpc.NewServer(opts...)
 	transport.RegisterGRPCServer(server, endpoints)
 	return server
 }
